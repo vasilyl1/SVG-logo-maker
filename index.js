37366253f111305a5f5ad6d24812c9b69e58d734
vasilyl1 <likhovaido@gmail.com>
2023-02-11 09:56:39 -0500

    further user input and init funcs

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const fs = require('fs'); // filesystem init
 const inq = require('inquirer'); // inquirer init
 
-function writeToFile(fileName, data) {
+function init(arg1, arg2, arg3) { // arg1 path arg2 filename arg3 -D switch
+    
+    console.log(
+        `**************************************\n
+         * SVG logo generator utility v 1.0   *\n
+         *            by vasilyl1             *\n
+         **************************************\n`
+        );
 
-    fs.readFile(fileName, function (error) { // attempt to read the file to check if it exists
-        if (error || (process.argv[2] === "-D")) { // if error or -D param - proceed to write/rewrite
-            userInterview(data, fileName);
+    fs.readFile(arg2, (error) => { // attempt to read the file to check if it exists
+        if (error || (arg3 === "-D")) { // if error or -D param - proceed to write/rewrite
+            userInterview(arg2);
         } else { // no error - the file exists
-            console.log(`Error: ${fileName} exists at path: ${process.argv[1].substring(0, process.argv[1].lastIndexOf("/") + 1)}. \n Re-run with -D to overwrite.`);
+            console.log(
+                `Error: ${arg2} exists at path: ${arg1.substring(0, arg1.lastIndexOf("/") + 1)}. \n
+                 Use -D to overwrite:\n
+                SYNTAX: node index.js <path\\filename> <-D>`);
             return;
         }
     }
@@ -17,9 +27,8 @@ function writeToFile(fileName, data) {
 
 }
 
-function userInterview(data, fileName) { // interview the user and generate the file content
+function userInterview(fileName) { // interview the user and generate the file content
     let correctInput = false;
-    console.log("Welcome to the SVG logo generator utility!");
     while (correctInput) {
         inq.prompt([
             {
@@ -31,7 +40,12 @@ function userInterview(data, fileName) { // interview the user and generate the 
         ])
             .then((response) => {
                 if ((response.logoText.length > 2) || (response.logoText === "")) {
-                    console.log("The logo can use no more than 3 symbols, please try again!");
+                    console.log(
+                        `The logo can use:\n
+                           - no more than 3 symbols,\n
+                           - at least 1 symbol\n
+                            Please try again!`
+                        );
                     return;
                 } else { // up to 3 symbols entered correctly
                     correctInput = True;
@@ -46,3 +60,8 @@ function userInterview(data, fileName) { // interview the user and generate the 
             });
     };
 }
+
+const arg1 = process.argv[1]; // gets app path here
+const arg2 = process.argv[2]; // expect to get filename here
+const arg3 = process.argv[3]; // expect to get -D param here
+init(arg1,arg2,arg3);
